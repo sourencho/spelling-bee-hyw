489e1a691d63e0f1e00fc8aca163d3724d7c072d
sourencho <souren@papazian.info>
2022-12-25 13:09:59 -0500

    Minor fixes

--- src/components/GameWon.vue
+++ src/components/GameWon.vue
@@ -1,14 +1,4 @@
-<script setup lang="ts">
-import Support from "./Support.vue";
-</script>
-
 <template>
-  <h2>You're a spelling Genius!</h2>
-  <Support />
-  <span
-    >If you liked this game, you might like the Irish version:
-    <el-link type="primary" href="https://beacha.ie" target="_blank">
-      beacha litríochta
-    </el-link>
-  </span>
+  <h2>Հանճարեղ!</h2>
 </template>
+
